136a90c077185c8e9bb1b72e0167edbf6cf8c098
luis rodriguez <kike.lerj2710@gmail.com>
2021-10-18 09:05:17 -0500

    agregar todas las imagnes al html

--- js/funciones.js
+++ js/funciones.js
@@ -36,7 +36,7 @@ const mostrarAlerta = (mensaje) =>{
 
 const buscarImagenes = termino => {
     const key = '23903726-f6955a57141d6856f725b7c8b';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=60`;
     fetch(url)
         .then(resultado => resultado.json())
         .then(respuesta =>{
@@ -46,10 +46,26 @@ const buscarImagenes = termino => {
 const mostrarImagenes = imagenes =>{
     console.log(imagenes);
 while (resultado.firstChild) {
-    resultado.removeChild(resultado.firstChild)
+    resultado.removeChild(resultado.firstChild);
+}
     imagenes.forEach(imagen => {
-    //  const {}= imagen
+        const {views, previewURL, largeImageURL , likes}= imagen
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img src="${previewURL}" class="w-full">
+                
+                <div class="p-4">
+                    <p class="font-bold"> ${likes} <span class="font-light">Me gusta </span></p>
+                    <p class="font-bold"> ${views} <span class="font-light">Visto </span></p>
+                    <a
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white font-bold uppercase text-center p-1 rounded mt-5"
+                         href="${largeImageURL}" target="_blank" rel="noopener noreferrer">ver Imagen
+                    <a/>            
+                </div>
+            </div>
+        </div>
+        `;
     });
 
-}
 };
